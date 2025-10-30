d730880ef106d62c1be4b15cae4233c145d54f76
Md Abdul Sohaib <sohaibmdabdul069@gmail.com>
2025-10-30 23:45:03 +0530

    change the  moble view to book ui

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration - MUST be before other middleware
 app.use(cors({
-  origin: ['https://7gramm.netlify.app', 'http://localhost:5173'],
+  origin: ['https://7gramm.netlify.app', 'http://localhost:5173','https://7grammbook.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], // Added PATCH
   allowedHeaders: ['Content-Type', 'Authorization']
